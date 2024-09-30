acca13ee5ae263e80aa6e53c3d160b5b22e45262
GustavoEnzo <93792149+GustavoEnzo@users.noreply.github.com>
2024-09-30 04:40:13 -0300

    animacao
    animacao do texto e recução do tempo

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,16 @@ import logo from './assets/imgs/logo.svg';
 function App() {
   const [isVisible, setIsVisible] = useState(false);
   const [text, setText] = useState('');
-  const description = " Caracol log é um aplicativo inovador de crowdshipping que conecta viajantes com espaço ocioso em suas malas a consumidores que precisam enviar ou receber pequenas encomendas.";
+  const [title, setTitle] = useState('');
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+  const description = "Caracol log é um aplicativo inovador de crowdshipping que conecta viajantes com espaço ocioso em suas malas a consumidores que precisam enviar ou receber pequenas encomendas.";
+  const fullTitle = "Baixe Nosso App";
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
-    }, 800);
+    }, 300);
 
     return () => clearTimeout(timer);
   }, []);
@@ -19,12 +23,20 @@ function App() {
     if (isVisible) {
       let index = 0;
       const typingTimer = setInterval(() => {
-        if (index < description.length) {
-          setText((prev) => prev + description.charAt(index));
-          index++;
-        } else {
-          clearInterval(typingTimer);
+        if (index <= fullTitle.length) {
+          setTitle((prev) => fullTitle.substring(0, index));
         }
+
+        if (index <= description.length) {
+          setText((prev) => description.substring(0, index));
+        }
+
+        if (index >= fullTitle.length && index >= description.length) {
+          clearInterval(typingTimer);
+          setIsButtonVisible(true); 
+        }
+
+        index++;
       }, 30);
 
       return () => clearInterval(typingTimer);
@@ -43,37 +55,37 @@ function App() {
               className={`w-full ml-7 md:w-1/2 h-auto order-1 md:order-2 transition-opacity duration-700 ${isVisible ? 'animate-slideIn opacity-100' : 'opacity-0'}`} 
             />
             <div className="text-center md:text-left w-auto md:w-7/12 order-2 md:order-1">
-              <h1 className="text-xl font-bold text-[#ffd600] md:text-3xl mb-2">Baixe Nosso App</h1>
+              <h1 className="text-xl font-bold text-[#ffd600] md:text-3xl mb-2">{title}</h1>
               <p className="font-light leading-relaxed text-sm md:text-lg text-white mb-4">{text}</p>
-              <a href="https://drive.usercontent.google.com/download?id=1Sbx6Gg_tX3E9l5bM0k0brLGdRRlDwFR5&export=download&authuser=0" download >
-                <button className="text-[rgb(233,221,60)] font-semibold border-2 border-whiite rounded-xl px-4 py-2 mb-4 shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-[rgb(233,221,60)]">
-                  Faça o Download
-                </button>
-              </a>
+              {isButtonVisible && (
+                <a href="https://drive.usercontent.google.com/download?id=1Sbx6Gg_tX3E9l5bM0k0brLGdRRlDwFR5&export=download&authuser=0" download >
+                  <button className="text-[rgb(233,221,60)] font-semibold border-2 border-whiite rounded-xl px-4 py-2 mb-4 shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:border-[rgb(233,221,60)]">
+                    Faça o Download
+                  </button>
+                </a>
+              )}
             </div>
           </div>
         </div>
-        <footer className="  w-full flex flex-col gap-3 p-4 text-center">
-        <h1 className="text-xl font-normal text-[#ffff] mb-2">Redes Sociais</h1>
-        <div className="flex justify-center ">
-          <a href="https://www.instagram.com/caracologs" target="_blank" rel="noreferrer">
-            <img src="https://img.icons8.com/ios/50/EBEBEB/instagram-new--v1.png" alt="instagram" />
-          </a>
-        </div>
-        <p className="font-light text-white text-xs md:text-sm">© 2024 Caracol Log. Todos os direitos reservados.</p>
-      </footer>
+        <footer className="w-full flex flex-col gap-3 p-4 text-center">
+          <h1 className="text-xl font-normal text-[#ffff] mb-2">Redes Sociais</h1>
+          <div className="flex justify-center ">
+            <a href="https://www.instagram.com/caracologs" target="_blank" rel="noreferrer">
+              <img src="https://img.icons8.com/ios/50/EBEBEB/instagram-new--v1.png" alt="instagram" />
+            </a>
+          </div>
+          <p className="font-light text-white text-xs md:text-sm">© 2024 Caracol Log. Todos os direitos reservados.</p>
+        </footer>
       </div>
-
-   
 
       <style>
         {`
-          @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+          @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap');
 
           body {
             font-family: 'Poppins', sans-serif;
-            }
-            
+          }
+
           @keyframes slideIn {
             from {
               transform: translateX(100%);
